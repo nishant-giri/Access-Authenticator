c2eebca5f26811a61bb9d9b5630b8db30abbb0c3
Simran Verma <88669720+Simranverma123@users.noreply.github.com>
2023-05-02 19:50:18 +0530

    Added a file.

---
+++ notify.py
@@ -0,0 +1,16 @@
+from twilio.rest import Client
+
+
+def sendmsg():
+
+    account_sid = 'YOUR ACCOUNT SID'
+    auth_token = 'YOUR AUTH TOKEN'
+    client = Client(account_sid, auth_token)
+
+    message = client.messages.create(
+        from_='SENDER  NUMBER',
+        body='Unauthorized access in system',
+        to='RECEIVER NUMBER'
+    )
+
+    print(message.sid)
