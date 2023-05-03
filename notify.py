45cb9ba4700c34b0021be8fcb65d3a5987562e40
Nishant Giri <nishantgiri.20@gmail.com>
2023-05-03 17:59:30 +0530

    Update notify.py

--- notify.py
+++ notify.py
@@ -1,16 +1,14 @@
 from twilio.rest import Client
 
-
 def sendmsg():
-
     account_sid = 'YOUR ACCOUNT SID'
     auth_token = 'YOUR AUTH TOKEN'
     client = Client(account_sid, auth_token)
 
     message = client.messages.create(
-        from_='SENDER  NUMBER',
-        body='Unauthorized access in system',
-        to='RECEIVER NUMBER'
+        from_ = 'SENDER NUMBER',
+        body = 'Unauthorized access in system',
+        to = 'RECEIVER NUMBER'
     )
 
     print(message.sid)
